60ecac69d6912f4b0910052e1857961a5a60e7ad
Woerdnis <edwardreisch@gmail.com>
2025-10-27 21:05:53 +0100

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,11 +1,29 @@
-const slides = document.querySelectorAll('.slide');
-let currentSlide = 0;
+const slideshow = document.getElementById("slideshow");
 
-function nextSlide() {
-  slides[currentSlide].classList.remove('active');
-  currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].classList.add('active');
+const imageCount = 23;
+
+let images = [];
+for (let i = 1; i <= imageCount; i++) {
+  images.push(images/${i}.jpg);
 }
 
-slides[currentSlide].classList.add('active');
-setInterval(nextSlide, 5000);
+for (let i = images.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [images[i], images[j]] = [images[j], images[i]];
+}
+
+images.forEach((src, index) => {
+  const div = document.createElement("div");
+  div.classList.add("slide");
+  div.style.backgroundImage = url('${src}');
+  if (index === 0) div.classList.add("active");
+  slideshow.appendChild(div);
+});
+
+let current = 0;
+setInterval(() => {
+  const slides = document.querySelectorAll(".slide");
+  slides[current].classList.remove("active");
+  current = (current + 1) % slides.length;
+  slides[current].classList.add("active");
+  }, 5000); 
